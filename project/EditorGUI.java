e4b7bf257b0d201d5fdb64bc104849f14c34d1e8
mardaca8 <mardaca8@gmail.com>
2025-05-11 14:52:15 +0300

    editor options

--- project/EditorGUI.java
+++ project/EditorGUI.java
@@ -1,3 +1,4 @@
+
 import editor.*;
 import javax.swing.*;
 import java.awt.*;
@@ -11,9 +12,8 @@ public class EditorGUI extends JFrame {
     private JTextField inputField;
     private JLabel changesLabel;
     
-    
-
     public EditorGUI() {
+        
         JDialog popupDialog = new JDialog(this, "Editor", true);
         popupDialog.setSize(200, 100);
         popupDialog.setLayout(new FlowLayout());
@@ -26,17 +26,17 @@ public class EditorGUI extends JFrame {
 
         translateButton.addActionListener(e -> {
             editor = new TranslateEditor();
-            popupDialog.dispose(); 
+            popupDialog.dispose();
+            
         });
 
         spellCheckButton.addActionListener(e -> {
             editor = new SpellCheckEditor();
-            popupDialog.dispose(); 
+            popupDialog.dispose();
         });
 
         popupDialog.setLocationRelativeTo(this);
         popupDialog.setVisible(true);
-
         setTitle("Teksto Redaktorius");
         setSize(400, 400);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -91,6 +91,8 @@ public class EditorGUI extends JFrame {
         });
         setVisible(true);
     }
+
+    
 
     private void updateDisplay() {
         textArea.setText(editor.text);
